34514ecaf7345fb96267c5d81142ba34d67a10ab
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2022-12-17 10:57:29 -0400

    Solved issue with open street maps

--- lib/trade/ui/locationPage.dart
+++ lib/trade/ui/locationPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:project_trading/common/components/verticalSpac.dart';
+import 'package:project_trading/common/components/horizontalSpace.dart';
 import 'package:project_trading/common/sizes.dart';
+import 'package:project_trading/common/components/toast.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:project_trading/common/components/gpsAppBar.dart';
+import 'package:project_trading/common/components/verticalSpac.dart';
 
 class LocationsPage extends StatefulWidget {
   @override
@@ -13,22 +15,34 @@ class _LocationsPageState extends State<LocationsPage> {
   int _selectedIndex = 0;
   int? selectedDay;
   int? selectedMonth;
+  bool loadingSelection = true;
 
   MapController controller = MapController(
-    initMapWithUserPosition: false,
-    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
-    areaLimit: BoundingBox(
-      east: 10.4922941,
-      north: 47.8084648,
-      south: 45.817995,
-      west: 5.9559113,
-    ),
+    initMapWithUserPosition: true,
+    // initPosition: GeoPoint(latitude: 39.465038, longitude: -0.384516),
+    // areaLimit: BoundingBox(
+    //   east: 3.384516,
+    //   north: 42.465038,
+    //   south: 36.465038,
+    //   west: -3.384516,
+    // ),
   );
 
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      controller.advancedPositionPicker();
+      setState(() {
+        loadingSelection = false;
+      });
+    });
+    super.initState();
   }
 
   @override
@@ -51,17 +65,34 @@ class _LocationsPageState extends State<LocationsPage> {
               height: Sizes.width,
               child: OSMFlutter(
                 controller: controller,
-                trackMyPosition: false,
+                showZoomController: true,
+                trackMyPosition: true,
                 initZoom: 12,
                 minZoomLevel: 8,
                 maxZoomLevel: 14,
                 stepZoom: 1.0,
-                mapIsLoading: const Center(child: CircularProgressIndicator()),
-                onGeoPointClicked: (GeoPoint geo) {
-                  print("Click (${geo.latitude}, ${geo.longitude})");
+                mapIsLoading: Center(
+                    child: SizedBox(
+                        width: Sizes.padding/2,
+                        height: Sizes.padding/2,
+                        child: const CircularProgressIndicator())),
+                onLocationChanged: (myLocation) {
+                  print("My location is $myLocation");
                 },
-                onLocationChanged: (GeoPoint geo) {
-                  print("Change (${geo.latitude}, ${geo.longitude})");
+                onGeoPointClicked: (geoPoint) async {
+                  print("Clicked on $geoPoint");
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        geoPoint.toMap().toString(),
+                      ),
+                      action: SnackBarAction(
+                        onPressed: () =>
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+                        label: "hide",
+                      ),
+                    ),
+                  );
                 },
                 userLocationMarker: UserLocationMaker(
                   personMarker: const MarkerIcon(
@@ -98,42 +129,95 @@ class _LocationsPageState extends State<LocationsPage> {
                 )),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal:Sizes.padding, vertical: Sizes.padding/4),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "big sunova, Valencia",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text("Tap to select this location",
-                        style: TextStyle(fontWeight: FontWeight.normal, color: Colors.black38))
-                    ],
-                  ),
-                  Icon(Icons.arrow_forward)
-                ],
+            GestureDetector(
+              onTap: () async {
+                if (loadingSelection) {
+                  return;
+                }
+                setState(() {
+                  loadingSelection = true;
+                });
+                await Future.delayed(Duration(seconds: 4));
+                GeoPoint p =
+                    await controller.getCurrentPositionAdvancedPositionPicker();
+                controller.advancedPositionPicker();
+                print("Position $p");
+                showToast(
+                    "Ubicación seleccionada es ${p.latitude}, ${p.longitude}");
+                setState(() {
+                  loadingSelection = false;
+                });
+              },
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.padding, vertical: Sizes.padding / 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "big sunova, Valencia",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("Tap to select this location",
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black38))
+                      ],
+                    ),
+                    loadingSelection
+                        ? const CircularProgressIndicator()
+                        : const Icon(Icons.arrow_forward)
+                  ],
+                ),
               ),
             ),
             verticalSpace(),
-            Container(
-              width: double.infinity,
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: Sizes.padding),child: Text("Nerby Places"),),
-            verticalSpace(),
-            Container(
-              width: double.infinity,
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: Sizes.padding),child: Row(
-                children: [
-                  Icon(Icons.location_on_rounded, color: Colors.red,),
-                  Text("Valencia"),
-                ],
-              ),),
-            verticalSpace(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.symmetric(horizontal: Sizes.padding),
+                      child: const Text("Nerby Places"),
+                    ),
+                    verticalSpace(),
+                    positionTile(
+                        Icons.location_on_rounded, Colors.red, "Valencia"),
+                    verticalSpace(),
+                    positionTile(
+                        Icons.indeterminate_check_box_outlined,
+                        const Color.fromARGB(255, 19, 53, 82),
+                        "Corporación Inlaca, C.A."),
+                    verticalSpace(),
+                    positionTile(Icons.indeterminate_check_box_outlined,
+                        const Color.fromARGB(255, 19, 53, 82), "ximko.com"),
+                    verticalSpace(),
+                    positionTile(
+                        Icons.indeterminate_check_box_outlined,
+                        const Color.fromARGB(255, 19, 53, 82),
+                        "seguros caroni"),
+                    verticalSpace(),
+                    positionTile(
+                        Icons.indeterminate_check_box_outlined,
+                        const Color.fromARGB(255, 19, 53, 82),
+                        "Casa David Hogar"),
+                    verticalSpace(),
+                    positionTile(
+                        Icons.indeterminate_check_box_outlined,
+                        const Color.fromARGB(255, 19, 53, 82),
+                        "Corporación Inlaca, C.A."),
+                    verticalSpace(),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -186,5 +270,30 @@ class _LocationsPageState extends State<LocationsPage> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  Widget positionTile(IconData icon, Color color, String message) {
+    return GestureDetector(
+      onTap: () {
+        GeoPoint p = GeoPoint(latitude: 39.457087, longitude: -0.388293);
+        controller.goToLocation(p);
+      },
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.centerLeft,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: Sizes.padding),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: color,
+            ),
+            horizontalSpace(),
+            Text(message),
+          ],
+        ),
+      ),
+    );
   }
 }
